2e5453c050ce1a8e999faee446b2b63873547f07
jasminko <betelge@gmail.com>
2012-03-26 20:01:39 +0000

    New FrustumVolume class. Added CameraNode.generateFrustumVolume(). Made radius of SphereVolume private.

--- src/betel/alw3d/renderer/CameraNode.java
+++ src/betel/alw3d/renderer/CameraNode.java
@@ -1,5 +1,8 @@
 package betel.alw3d.renderer;
 
+import betel.alw3d.FrustumVolume;
+import betel.alw3d.math.Vector3f;
+
 public class CameraNode extends MovableNode {
 	float fov, aspect, zNear, zFar;
 	
@@ -8,6 +11,44 @@ public class CameraNode extends MovableNode {
 		this.aspect = aspect;
 		this.zNear = zNear;
 		this.zFar = zFar;
+	}
+	
+	public FrustumVolume generateFrustumVolume() {
+		Vector3f[] normals = new Vector3f[6];
+		float[] distances = new float[6];
+		
+		// Vertical fov in radians
+		float rfov = fov * (float)Math.PI/180;
+		
+		// Top
+		normals[0] = new Vector3f(0f, -(float)Math.cos(rfov/2), -(float)Math.sin(rfov/2));
+		distances[0] = 0;
+		
+		// Bottom
+		normals[1] = new Vector3f(0f, (float)Math.cos(-rfov/2), (float)Math.sin(-rfov/2));
+		distances[1] = 0;
+		
+		// Near
+		normals[2] = new Vector3f(0f, 0f, -1f);
+		distances[2] = zNear;
+		
+		// Far
+		normals[3] = new Vector3f(0f, 0f, 1f);
+		distances[3] = -zFar;
+		
+		// Horizontal fov in radians
+		// TODO: aspect is unknown here
+		float hfov = 2*(float)Math.cos(/*aspect*/Math.acos(rfov/2) / 2);
+		
+		// Left
+		normals[4] = new Vector3f((float)Math.cos(hfov/2), 0f,-(float)Math.sin(hfov/2));
+		distances[4] = 0;
+		
+		// Right
+		normals[5] = new Vector3f(-(float)Math.cos(-hfov/2), 0f,(float)Math.sin(-hfov/2));
+		distances[5] = 0;
+		
+		return new FrustumVolume(normals, distances);
 	}
 
 	public float getFov() {
