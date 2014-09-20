51a98c2687987178311cb4fef158ccbcde7fd1bb
jasminko <jasminko@ccc6fceb-0532-4d14-8127-1cbe3967364c>
2014-09-20 20:30:14 +0000

    ShaderLoader can now include external add glsl code from extra files into shaders.

--- trunk/src/utils/ShaderLoader.java
+++ trunk/src/utils/ShaderLoader.java
@@ -1,17 +1,33 @@
 package utils;
 
-import java.io.IOException;
-
 import betel.alw3d.renderer.ShaderProgram;
 import betel.alw3d.renderer.ShaderProgram.Shader;
 
 public class ShaderLoader {
 	static public ShaderProgram loadShaderProgram(int vertex, int fragment) {
+		return loadShaderProgram(vertex, fragment, null, null);
+	}
+	
+	static public ShaderProgram loadShaderProgram(int vertex, int fragment, int[] includes) {
+		return loadShaderProgram(vertex, fragment, includes, includes);
+	}
+	
+	static public ShaderProgram loadShaderProgram(int vertex, int fragment,
+			int[] vertexIncludes, int[] fragmentIncludes) {
+		
 		Shader vertexShader = new Shader(Shader.Type.VERTEX, 
-				StringLoader.loadStringExceptionless(vertex));
+				StringLoader.loadStringExceptionless(vertex) + loadIncludes(vertexIncludes));
 		Shader fragmentShader = new Shader(Shader.Type.FRAGMENT, 
-				StringLoader.loadStringExceptionless(fragment));
+				StringLoader.loadStringExceptionless(fragment) + loadIncludes(fragmentIncludes));
 		
 		return new ShaderProgram(vertexShader, fragmentShader);
+	}
+	
+	static private String loadIncludes(int[] includes) {
+		String includeSource = "";
+		if(includes != null)
+			for(int include : includes)
+				includeSource += StringLoader.loadStringExceptionless(include);
+		return includeSource;
 	}
 }
